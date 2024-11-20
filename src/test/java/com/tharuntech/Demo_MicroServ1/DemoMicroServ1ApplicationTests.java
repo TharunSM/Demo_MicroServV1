d3493944fc252aa9b5ce134d429092e04bc8031f
THARUN <tharuntech9840@gmail.com>
2024-11-20 20:47:15 +0530

    starter files added

---
+++ src/test/java/com/tharuntech/Demo_MicroServ1/DemoMicroServ1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharuntech.Demo_MicroServ1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroServ1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
