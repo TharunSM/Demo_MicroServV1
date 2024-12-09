a7150a652b5e0ca0ffca9b1afd9a54d528911a98
THARUN <tharuntech9840@gmail.com>
2024-12-09 19:50:02 +0530

    added find RatingCount and findCurrentRating Repo Methods

--- src/main/java/com/tharuntech/Demo_MicroServ1/repository/RateingRepo.java
+++ src/main/java/com/tharuntech/Demo_MicroServ1/repository/RateingRepo.java
@@ -1,12 +1,18 @@
 package com.tharuntech.Demo_MicroServ1.repository;
 
 import com.tharuntech.Demo_MicroServ1.entity.Model.BundleRateing;
-import com.tharuntech.Demo_MicroServ1.entity.Model.ValorantSkinHub;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface RateingRepo extends JpaRepository<BundleRateing, String> {
 
     Optional<BundleRateing> findByUuid(String uuid);
+
+    @Query("SELECT r.rateingCount FROM BundleRateing r WHERE r.uuid = :uuid")
+    Optional<Integer> findRatingCount(String uuid);
+
+    @Query("SELECT r.rateing FROM BundleRateing r WHERE r.uuid = :uuid")
+    Optional<Integer> findCurrentRating(String uuid);
 }
